3d2408bacf700a39f08ebe5e22821c36afa45f7a
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-05-09 23:25:41 +0330

    create Picture Uploader project

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import * as Vsc from "react-icons/vsc";
+
+function App() {
+  return (
+    <div className="App vh-100 d-flex flex-column justify-content-between">
+      <div className="main">
+          محتوا
+      </div>
+
+      <div className="footer text-center py-3">
+        <small> طراحی شده توسط <a href="https://hamidkamyab.ir/" target="_blank" className="text-muted">حمید کامیاب</a></small>
+      </div>
+    </div>
+  );
+}
+
+export default App;
